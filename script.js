b14a7aa35867578764b0aa5b8766036160caa8fa
AldiBragi <kadekaldy@gmail.com>
2025-04-11 19:41:50 +0800

    updute progres

--- script.js
+++ script.js
@@ -2,21 +2,6 @@ const hamburger = document.getElementById('hamburger');
 const sideMenu = document.getElementById('sideMenu');
 const closeBtn = document.getElementById('closeBtn');
 const mainContent = document.getElementById('mainContent');
-
-hamburger.addEventListener('click', () => {
-  sideMenu.classList.add('show');
-  mainContent.classList.add('blur');
-  hamburger.style.opacity = '0';
-});
-
-closeBtn.addEventListener('click', () => {
-  sideMenu.classList.remove('show');
-  mainContent.classList.remove('blur');
-  setTimeout(() => {
-    hamburger.style.opacity = '1';
-  }, 300); // sesuai waktu transisi menu keluar
-});
-
 const overlayBlur = document.getElementById('overlayBlur');
 
 hamburger.addEventListener('click', () => {
@@ -27,8 +12,16 @@ hamburger.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
   sideMenu.classList.remove('show');
+  mainContent.classList.remove('blur');
   overlayBlur.style.display = 'none';
   setTimeout(() => {
     hamburger.style.opacity = '1';
   }, 300);
+});
+
+hamburger.addEventListener('click', () => {
+  sideMenu.classList.add('show');
+  overlayBlur.style.display = 'block';
+  hamburger.style.opacity = '0';
+  mainContent.classList.add('blur'); // Tambahan efek blur
 });
